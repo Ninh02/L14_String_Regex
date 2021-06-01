f743c293430e7f7ee72963dec9cff62830af9298
Ninh02 <ninhdo10021998@gmail.com>
2021-06-02 06:24:26 +0700

    Validate Email

---
+++ BT/Validate_Email/Validate_email.php
@@ -0,0 +1,12 @@
+<?php
+function checkEmail($email){
+    $pattern="/^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/";
+    if(preg_match_all($pattern,$email)){
+        echo "Email hop le";
+    }else{
+        echo "Email khong hop le";
+    }
+}
+checkEmail("a@gmail.com");
+echo "<br>";
+checkEmail("ab@gmail.");
