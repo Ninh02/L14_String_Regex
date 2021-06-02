9b29ec2154988d181c2b15a24479bbf1008760c0
Ninh02 <ninhdo10021998@gmail.com>
2021-06-02 18:52:57 +0700

    So dien thoai

--- BT/So_dien_thoai/So_dien_thoai.php
+++ BT/So_dien_thoai/So_dien_thoai.php
@@ -1,1 +1,12 @@
 <?php
+function checkPhone($phone){
+    $pattern="/^[0]+[0-9]{9}$/";
+    if(preg_match_all($pattern,$phone)){
+        echo "so hop le";
+    }else{
+        echo "so khong hop le";
+    }
+}
+checkPhone("0362223456");
+echo "<br>";
+checkPhone("1234567898");
