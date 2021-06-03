f11fd41c975e2e0c6b9e2921e2750d4ee942aa89
Ninh02 <ninhdo10021998@gmail.com>
2021-06-03 09:23:05 +0700

    so dien thoai

--- BT/So_dien_thoai/So_dien_thoai.php
+++ BT/So_dien_thoai/So_dien_thoai.php
@@ -1,6 +1,6 @@
 <?php
 function checkPhone($phone){
-    $pattern="/^[0]+[0-9]{9}$/";
+    $pattern="/^0\d{9}$/";
     if(preg_match_all($pattern,$phone)){
         echo "so dien thoai hop le";
     }else{
